f78ec5b7c88118e8a14225b3fbb7eff45b25e8fa
D_Zucho <danny.nico.dev@outlook.com>
2020-02-11 05:26:51 -0800

    add simple css & form submit handler

---
+++ main.js
@@ -0,0 +1,27 @@
+const notes = []
+
+const filters = {
+    filterText: ''
+}
+
+
+
+
+
+
+// Add new todo to list
+document.querySelector('#new-note-form').addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    //assign input text as note title
+    const noteTitle = e.target.elements.noteTitleText.value
+
+    // add note object to notes array
+    notes.push({
+        title: noteTitle,
+        body: ''
+    })
+    e.target.elements.noteTitleText.value = ''
+
+
+})
