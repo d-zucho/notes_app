6d76958c30186f78402fc7e8e5dd2fe0fa632755
D_Zucho <danny.nico.dev@outlook.com>
2020-02-13 04:52:47 -0800

    render notes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const filters = {
 	filterText: '',
 };
 
-// Add new todo to list
+/**  -- Add new todo to list */
 document.querySelector('#new-note-form').addEventListener('submit', function(e) {
 	e.preventDefault();
 
@@ -17,4 +17,28 @@ document.querySelector('#new-note-form').addEventListener('submit', function(e) 
 		body: '',
 	});
 	e.target.elements.noteTitleText.value = '';
+
+	document.querySelector('#notes').innerHTML = '';
+	renderNotes(notes, filters);
 });
+
+/**  ---  Display Notes  ---  */
+const renderNotes = function(notes, filters) {
+	notes.forEach(note => {
+		const noteEl = document.createElement('div');
+		const txt = document.createElement('span');
+		txt.textContent = note.title;
+
+		const editButton = document.createElement('button');
+		editButton.textContent = 'edit';
+
+		const deleteButton = document.createElement('button');
+		deleteButton.textContent = 'x';
+
+		noteEl.appendChild(txt);
+		noteEl.appendChild(editButton);
+		noteEl.appendChild(deleteButton);
+
+		document.querySelector('#notes').appendChild(noteEl);
+	});
+};
