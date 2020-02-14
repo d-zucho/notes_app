8e88346a50baa9f5b89083060e449ebe52dde0cf
D_Zucho <danny.nico.dev@outlook.com>
2020-02-14 08:14:29 -0800

    work on localStorage

--- main.js
+++ main.js
@@ -1,9 +1,47 @@
-const notes = [];
+let notes = [];
 
 const filters = {
 	filterText: '',
 };
 
+// Check for existing saved Data
+const notesJSON = localStorage.getItem('notes');
+
+if (notesJSON !== null) {
+	notes = JSON.parse(notesJSON);
+}
+
+/**  ---  Display Notes  ---  */
+const renderNotes = function(notes, filters) {
+	const filteredNoted = notes.filter(function(note) {
+		return note.title.toLowerCase().includes(filters.filterText.toLowerCase());
+	});
+
+	filteredNoted.forEach(note => {
+		const noteEl = document.createElement('div');
+		const txt = document.createElement('span');
+
+		if (note.title.length > 0) {
+			txt.textContent = note.title;
+		} else {
+			txt.textContent = 'Unnamed Note';
+		}
+
+		//create note DOM object
+		const editButton = document.createElement('button');
+		editButton.textContent = 'edit';
+		const deleteButton = document.createElement('button');
+		deleteButton.textContent = 'x';
+
+		noteEl.appendChild(txt);
+		noteEl.appendChild(editButton);
+		noteEl.appendChild(deleteButton);
+
+		document.querySelector('#notes').appendChild(noteEl);
+	});
+};
+
+renderNotes(notes, filters);
 /**  -- Add new todo to list */
 document.querySelector('#new-note-form').addEventListener('submit', function(e) {
 	e.preventDefault();
@@ -17,29 +55,17 @@ document.querySelector('#new-note-form').addEventListener('submit', function(e) 
 		title: noteTitle,
 		body: '',
 	});
+
+	localStorage.setItem('notes', JSON.stringify(notes));
+
 	e.target.elements.noteTitleText.value = '';
 
 	document.querySelector('#notes').innerHTML = '';
 	renderNotes(notes, filters);
 });
 
-/**  ---  Display Notes  ---  */
-const renderNotes = function(notes, filters) {
-	notes.forEach(note => {
-		const noteEl = document.createElement('div');
-		const txt = document.createElement('span');
-		txt.textContent = note.title;
-
-		const editButton = document.createElement('button');
-		editButton.textContent = 'edit';
-
-		const deleteButton = document.createElement('button');
-		deleteButton.textContent = 'x';
-
-		noteEl.appendChild(txt);
-		noteEl.appendChild(editButton);
-		noteEl.appendChild(deleteButton);
-
-		document.querySelector('#notes').appendChild(noteEl);
-	});
-};
+/**  -- Listen for filterText --   */
+document.querySelector('#filterText').addEventListener('input', function(e) {
+	filters.filterText = e.target.value;
+	renderNotes(notes, filters);
+});
